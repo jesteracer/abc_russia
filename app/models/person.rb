4ec7a3f73c427f4c2ccc2bd9ed7cab68a9e67e4e
jesteracer <jesteracer@me.com>
2013-06-26 23:35:02 +0400

    Destroy person dependant resources

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,8 @@
 class Person < ActiveRecord::Base
-  include ActiveModel::ForbiddenAttributesProtection
-  # attr_accessible :about, :accounts, :accused_for, :birth_date, :contacts, :fio
-  has_many :curations
-  has_many :photos
-  has_many :transactions
-  has_many :connections
+  has_many :curations, dependent: :destroy
+  has_many :photos, dependent: :destroy
+  has_many :transactions, dependent: :destroy
+  has_many :connections, dependent: :destroy
   has_many :posts, through: :connections
 
   def curator?(user)
