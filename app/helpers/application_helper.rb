878058cf95c43a6d711775c18d39977588491915
jesteracer <jesteracer@me.com>
2013-07-05 18:43:40 +0400

    Refactoring in posts - rendering via partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def markdown!(marked)
     marked = marked.to_s
     marked.sub!('[-more-]','')
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true).render(marked).html_safe
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true).render(marked).html_safe
   end
 end
